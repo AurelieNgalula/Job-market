0513aa4230f67e2f2510d834b370576dee4db567
Yaoyao <pokemon.yy.go.on@gmail.com>
2026-01-23 17:13:14 +0000

    add parametre range

--- script/requete.py
+++ script/requete.py
@@ -2,6 +2,7 @@ import http.client
 import json 
 import requests
 from utils import get_access_token
+import time
 
 #utlis.py
 access_token = get_access_token()
@@ -13,16 +14,44 @@ headers = {
     'Accept': "application/json"
 }
 
-conn.request("GET", "/partenaire/offresdemploi/v2/offres/search?typeContrat=CDI&tempsPlein=true&departement=75", headers=headers)
+batch_size = 150
+start = 0
+all_jobs = []
 
-res = conn.getresponse()
-data = res.read()
+while True:
 
-# Décodage des données JSON
-data_json = json.loads(data.decode("utf-8"))
+    end=start + batch_size - 1
+    conn.request("GET", f"/partenaire/offresdemploi/v2/offres/search?typeContrat=CDI&tempsPlein=true&departement=75&range={start}-{end}", headers=headers)
+
+    res = conn.getresponse()
+
+    data = res.read()
+
+    data_json = json.loads(data.decode("utf-8"))
+
+    jobs = data_json.get("resultats", [])
+
+    if not jobs:
+        break
+
+    all_jobs.extend(jobs)
+
+    content_range = res.getheader("Content-Range")
+    try:
+        total_jobs = int(content_range.split("/")[1])
+
+    except (IndexError, ValueError, TypeError):
+        total_jobs = len(all_jobs)
+
+    start += batch_size
+
+    if start >= total_jobs:
+        break
+
+    time.sleep(0.5)  # Pour éviter de surcharger l'API
 
 # Sauvegarde dans un fichier JSON
 with open("out/offres_emploi.json", "w", encoding="utf-8") as f:
-    json.dump(data_json, f, ensure_ascii=False, indent=4)
-
-print("Les résultats ont été sauvegardés dans 'offres_emploi.json'.")
+    json.dump(all_jobs, f, ensure_ascii=False, indent=4)
+    print(f"Total des offres récupérées: {len(all_jobs)}")
+    print("Les résultats ont été sauvegardés dans 'offres_emploi.json'.")
